27f0093f6ca3a27bb4224b4a90d79219610a863e
rohitgr7 <rohitgr1998@gmail.com>
2021-09-28 17:43:21 +0530

    fix tests

--- pytorch_lightning/loops/optimization/optimizer_loop.py
+++ pytorch_lightning/loops/optimization/optimizer_loop.py
@@ -494,6 +494,9 @@ class OptimizerLoop(Loop):
 
         # clip gradients
         self.trainer.lightning_module.configure_gradient_clipping(
-            optimizer, opt_idx, self.trainer.gradient_clip_val, self.trainer.gradient_clip_algorithm
+            optimizer,
+            opt_idx,
+            gradient_clip_val=self.trainer.gradient_clip_val,
+            gradient_clip_algorithm=self.trainer.gradient_clip_algorithm,
         )
         return grad_norm_dict
